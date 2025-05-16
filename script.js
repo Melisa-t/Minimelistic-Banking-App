98b1c266f916773e152d25d22317d755d8280b47
Melisa Lachev <melisatormac55@gmail.com>
2025-05-16 17:04:15 +0300

    added all DOM el

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ let transferTo = document.getElementById("transfer-to").value;
 let transferAmount = document.getElementById("transfer-amount").value;
 const transferBtn = document.querySelector(`.transfer-btn`);
 
-let requestAmount = document.getElementById("request.amount").value
-const requestBtn = document.querySelector(`.request-btn`)
+let requestAmount = document.getElementById("request.amount").value;
+const requestBtn = document.querySelector(`.request-btn`);
 
 let confirmUser = document.getElementById("confirm-user").value;
 let confirmPIN = document.getElementById("confirm-pin").value;
+
+let activityNo = document.querySelector(`.activity-no`);
+let activityDate = document.querySelector(`.activity-date`);
+let activityAmount = document.querySelector(`.activity-amount`);
