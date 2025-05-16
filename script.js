b7c29a4daa0031f76ef515e779d0436621b816ec
Melisa Lachev <melisatormac55@gmail.com>
2025-05-16 16:58:44 +0300

    added summary

---
+++ script.js
@@ -0,0 +1,18 @@
+const userInput = document.querySelector(`.input-name`);
+const userPass = document.querySelector(`.pass`);
+
+const date = document.querySelector(`.date`);
+
+const currentBalance = document.querySelector(`.current-balance`);
+
+const bankActivities = document.querySelector(`.bank-activities`);
+
+let transferTo = document.getElementById("transfer-to").value;
+let transferAmount = document.getElementById("transfer-amount").value;
+const transferBtn = document.querySelector(`.transfer-btn`);
+
+let requestAmount = document.getElementById("request.amount").value
+const requestBtn = document.querySelector(`.request-btn`)
+
+let confirmUser = document.getElementById("confirm-user").value;
+let confirmPIN = document.getElementById("confirm-pin").value;
